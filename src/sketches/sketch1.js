24e552d6213273b2042e8eae0a8b287ef045bfe9
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-06-15 18:21:08 +0200

    feat: improve visualization

--- src/sketches/sketch1.js
+++ src/sketches/sketch1.js
@@ -1,14 +1,63 @@
 // @ts-nocheck
+import P5 from 'p5';
 
 import { waveTypes, colors } from '../utility';
 
-let t = 0;
-
-const wave = [];
-
 // fourier series
 
-const sketch1 = options => (ctx) => {
+const sketch1 = (ctx) => {
+  let t = 0;
+  const wave = [];
+  let wave2 = [];
+
+  const $step = document.querySelector('input[name="step"]');
+  const $iterations = document.querySelector('input[name="iterations"]');
+  const $waveType = document.querySelector('select[name="waveType"]');
+
+  // Create wave type select element
+  Object.keys(waveTypes).forEach((waveType) => {
+    const $option = document.createElement('option');
+    $option.value = waveType;
+    $option.text = waveType;
+    $waveType.appendChild($option);
+  });
+
+  const options = {
+    step: Number($step.value),
+    iterations: Number($iterations.value),
+    width: () => window.innerWidth,
+    height: () => 500,
+    waveType: $waveType.value,
+  };
+
+  const change = () => {
+    wave2 = [];
+  };
+
+  // Listen for changes
+  $step.addEventListener('change', (e) => {
+    e.preventDefault();
+    options.step = Number(e.target.value);
+
+    change();
+  });
+
+  $iterations.addEventListener('change', (e) => {
+    e.preventDefault();
+    options.iterations = Number(e.target.value);
+
+    change();
+  });
+
+  $waveType.addEventListener('change', (e) => {
+    e.preventDefault();
+    options.waveType = e.target.value;
+
+    change();
+  });
+
+  // First sketch init and run
+
   ctx.setup = () => {
     ctx.createCanvas(options.width(), options.height());
   };
@@ -21,7 +70,7 @@ const sketch1 = options => (ctx) => {
   };
 
   ctx.draw = () => {
-    ctx.background(0);
+    ctx.background(10, 10, 20);
 
     let x = 0;
     let y = 0;
@@ -54,15 +103,21 @@ const sketch1 = options => (ctx) => {
 
       const [r, v, b] = colors[i % colors.length];
 
+      if (i === 0) {
+        ctx.stroke(r, v, b);
+        ctx.strokeWeight(4);
+        ctx.point(0, 0);
+      }
+
       // circle
-      ctx.stroke(r, v, b, 128);
+      ctx.stroke(r, v, b, 192);
       ctx.strokeWeight(2);
       ctx.noFill();
       ctx.ellipse(px, py, rad * 2);
 
       // line
-      ctx.stroke(r, v, b);
-      ctx.strokeWeight(1);
+      ctx.stroke(r, v, b, 128);
+      ctx.strokeWeight(2);
       ctx.line(px, py, x, y);
 
       // point
@@ -70,21 +125,26 @@ const sketch1 = options => (ctx) => {
       ctx.point(x, y);
     }
 
+    wave2.unshift({ x, y });
     wave.unshift(y);
 
     if (wave.length > maxWaveSamples) {
       wave.pop();
     }
 
+    if (wave2.length > 1024) {
+      wave2.pop();
+    }
+
     // connected last circle center to the wave line
     ctx.stroke(255);
-    ctx.strokeWeight(1);
+    ctx.strokeWeight(2);
     ctx.line(x, y, waveOffsetX, wave[0]);
 
     // draw wave
 
     ctx.beginShape();
-    ctx.strokeWeight(1);
+    ctx.strokeWeight(2);
     ctx.stroke(255);
     ctx.noFill();
     for (let i = 0, m = wave.length; i < m; i += 1) {
@@ -92,8 +152,17 @@ const sketch1 = options => (ctx) => {
     }
     ctx.endShape();
 
+    ctx.beginShape();
+    ctx.strokeWeight(2);
+    ctx.stroke(255, 255, 255, 32);
+    ctx.noFill();
+    for (let i = 0, m = wave2.length; i < m; i += 1) {
+      ctx.vertex(wave2[i].x, wave2[i].y);
+    }
+    ctx.endShape();
+
     t -= options.step;
   };
 };
 
-export default sketch1;
+new P5(sketch1, document.getElementById('sketch1'));
--- src/utility.js
+++ src/utility.js
@@ -5,6 +5,7 @@ export const waveTypes = {
     if (!(n % 2)) return 0;
     return (n % 4 === 1 ? 1 : -1) / (n * n);
   },
+  sawtooth: n => (n % 2 === 1 ? -1 : 1) / (n + 1),
 };
 
 export const colors = [
@@ -16,6 +17,5 @@ export const colors = [
   [69, 170, 242],
   [75, 123, 236],
   [165, 94, 234],
-  [209, 216, 224],
   [119, 140, 163],
 ];
